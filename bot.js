d90369b83e8acb6423bea3cd64af7e5a82b31337
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-27 01:43:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,67 @@ client.on('guildMemberAdd', member => {
      welcomer.send({embed:yumz});          
          
     
+////////////////////////////////////////
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("505503857208459264");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
 
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("505526727108198411");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("505503857208459264");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+////////////////////////////////////////////////
+
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('505525914189430784');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
+});
+////////////////////////////////////////////////////
+
+
+ 
+
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose: ** Welcom To **__Stuning .__** Server **  `) 
+}).catch(console.error)
+})
+///////////////////////////////////////////
 
 
 
