1c9549bcb3d439d9e16215413dfbe3e621cf8422
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-11-11 19:36:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -876,7 +876,7 @@ message.author.send(`
 **
 ─════════════ {✯اوامر اداريه✯} ════════════─
 ❧ !bc ➺ برودكاست ب امبيد وبدون
-❧ !Link ➺ رابط انفايت للسيرفر
+❧ !link ➺ رابط انفايت للسيرفر
 ❧ !bans ➺ يقولك عدد الاشخاص المبندين من السيرفر
 ❧ !kick ➺ طرد عضو من السيرفر
 ❧ !ban ➺ تبنيد عضو من السيرفر
@@ -996,71 +996,5 @@ welcomer.sendFile(canvas.toBuffer())
 });
     
 /////////////////////////////
-client.on('message', message => { 
-  var sender = message.author
 
-if(!message.guild) return
-  
-  onoff: 'Off',
-  ch: 'Welcome',
-  msk: 'Welcome'
-  }
-
-if(message.content.startsWith(prefix + `set-wlc`)) {
-         
-  let perms = message.member.hasPermission(`MANAGE_CHANNELS`)
-
-  if(!perms) return message.channel.send('**You need `Manage Channels` permission**')
-  let args = message.content.split(" ").slice(1)
-  if(!args.join(" ")) return message.reply(`
-  ${prefix}set-wlc toggle 
-   ${prefix}set-wlc set [Channel Name]
-   ${prefix}set-wlc msg [Welcome Message]`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
-  let state = args[0]
-  if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'set' || !state.trim().toLowerCase() == 'msg' ) return message.reply(`
-  ${prefix}set-wlc toggle 
-  ${prefix}set-wlc set [Channel Name]
-  ${prefix}set-wlc msg [Welcome Message]`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
-    if(state.trim().toLowerCase() == 'toggle') { 
-     if(sw[message.guild.id].onoff === 'Off') return [message.channel.send(`**Welcome Message Is **ON** !**`), sw[message.guild.id].onoff = 'On']
-     if(sw[message.guild.id].onoff === 'On') return [message.channel.send(`**Welcome Message Is **OFF** !**`), sw[message.guild.id].onoff = 'Off']
-    }
-   if(state.trim().toLowerCase() == 'set') {
-   let newch = message.content.split(" ").slice(2).join(" ")
-   if(!newch) return message.reply(`${prefix}set-wlc set [Channel name]`)
-     if(!message.guild.channels.find(`name`,newch)) return message.reply(`I Cant Find This Channel.`)
-    sw[message.guild.id].ch = newch
-     message.channel.send(`**Welcome channel Has Been Changed to ${newch}.**`)
-   } 
-   if(state.trim().toLowerCase() == 'msg') {
-    let newmsg = message.content.split(" ").slice(2).join(" ")
-    if(!newmsg) return message.reply(`${prefix}set-wlc msg [New Message]`)
-     sw[message.guild.id].msk = newmsg
-      message.channel.send(`**Welcome message Has Been Changed to ${newmsg}.**`)
-    } 
-         }
-if(message.content === prefix + 'set-wlc info') {
-    let perms = message.member.hasPermission(`MANAGE_GUILD`) 
-    if(!perms) return message.reply(`You don't have permissions.`)
-    var embed = new Discord.RichEmbed()
-
-.addField(`Welcome message  `, `
-
-On/Off : __${sw[message.guild.id].onoff}__
-Channel : __${sw[message.guild.id].ch}__
-Message : __${sw[message.guild.id].msk}__`)
-
-
-    .setColor(`BLUE`)
-    message.channel.send({embed})
-  }
-
-  
-
-    fs.writeFile("./setwlc.json", JSON.stringify(sw), (err) => {
-    if (err) console.error(err)
-  });
-
-
-})
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
