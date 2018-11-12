f6b5fe5135105eba1b120c99833cfc11bf0e6a94
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-11-12 13:03:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1025,15 +1025,7 @@ welcomer.sendFile(canvas.toBuffer())
       });                    
  }
 });
-const wait = require('util').promisify(setTimeout);
-client.on('ready', () => {
-  wait(1000);
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-     
-    });
-  });
-});
+
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
